e83b60f154ea0ae210ac9169ff3eb31ac4b3eef8
Aurore David <aurore.david@acensi.local>
2017-12-06 17:12:43 +0100

    last commit 06-12 session, flash message etc.

--- src/OC/Platform2Bundle/Controller/AdvertController.php
+++ src/OC/Platform2Bundle/Controller/AdvertController.php
@@ -3,8 +3,11 @@
 namespace OC\Platform2Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+//use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+//use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AdvertController extends Controller
 {
@@ -34,13 +37,9 @@ class AdvertController extends Controller
     }
     public function viewAction($id)
     {
-        return $this->render(
-            '@OCPlatform2/Advert/index.html.twig', array(
-                "advert_id" => 10,
-                "nom" => "Aurore"
-            )
-        );
-//        return new Response("Affichage de l'annonce d'id : ".$id);
+        return $this->render('@OCPlatform2/Advert/view.html.twig', array(
+            'id' => $id
+        ));
     }
 
     public function viewSlugAction($slug, $year, $format)
@@ -49,5 +48,22 @@ class AdvertController extends Controller
             "On pourrait afficher l'annonce correspondant au
             slug '".$slug."', créée en ".$year." et au format ".$format."."
         );
+    }
+
+    public function addAction(Request $request)
+    {
+        $session = $request->getSession();
+
+        // Bien sûr, cette méthode devra réellement ajouter l'annonce
+
+        // Mais faisons comme si c'était le cas
+        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
+
+        // Le « flashBag » est ce qui contient les messages flash dans la session
+        // Il peut bien sûr contenir plusieurs messages :
+        $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
+
+        // Puis on redirige vers la page de visualisation de cette annonce
+        return $this->redirectToRoute('oc_platform_view', array('id' => 5));
     }
 }
