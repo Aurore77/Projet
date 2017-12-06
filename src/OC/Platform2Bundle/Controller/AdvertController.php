a03aa1d2c985db7b4b9506a6bc748ebfad527dcb
Aurore David <aurore.david@acensi.local>
2017-12-06 15:48:55 +0100

    modification des routes et ajout d'un préfixe de route dans le routing de app + modification des méthodes du controller

--- src/OC/Platform2Bundle/Controller/AdvertController.php
+++ src/OC/Platform2Bundle/Controller/AdvertController.php
@@ -4,20 +4,43 @@ namespace OC\Platform2Bundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class AdvertController extends Controller
 {
     public function indexAction()
     {
-        return $this->render(
-            '@OCPlatform2/Advert/index.html.twig',
-            array(
-                "nom" => "Aurore"
-            ));
+        // Obtenir une URL relative /platform/advert/id
+        // Méthode longue
+        /*$url = $this->get('router')->generate(
+            'oc_platform_view',
+            array('id' => 5)
+        );*/
+        // Méthode courte
+        $url = $this->generateUrl('oc_platform_home');
+
+        // Obtenir une URL absolue avec http://...
+        /*$url = $this->get('router')->generate(
+            'oc_platform_home', array(), UrlGeneratorInterface::ABSOLUTE_URL
+        );*/
+
+
+        return new Response("l'url de l'annonce d'id 5 est : ".$url);
+//        return $this->render(
+//            '@OCPlatform2/Advert/index.html.twig',
+//            array(
+//                "nom" => "Aurore"
+//            ));
     }
     public function viewAction($id)
     {
-        return new Response("Affichage de l'annonce d'id : ".$id);
+        return $this->render(
+            '@OCPlatform2/Advert/index.html.twig', array(
+                "advert_id" => 10,
+                "nom" => "Aurore"
+            )
+        );
+//        return new Response("Affichage de l'annonce d'id : ".$id);
     }
 
     public function viewSlugAction($slug, $year, $format)
